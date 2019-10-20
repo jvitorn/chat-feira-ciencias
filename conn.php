8c168c08f58f9174333692f9374c5f54e4d8932f
Jhonatancs <jdc_santos@outlook.com>
2019-10-20 15:57:26 -0300

    corrigindo erro de conexao

--- conn.php
+++ conn.php
@@ -2,7 +2,7 @@
 	mysqli_report(MYSQLI_REPORT_STRICT);
 	$conn = null;
 	try {
-	    $mysqli = new mysqli('127.0.0.1','root','usbw','tacs2');
+	    $conn = new mysqli('127.0.0.1','root','usbw','tacs2');
 	} catch (Exception $e) {
 
 		try{
